35667c562ef995331870a9bc74619788be7b15a7
valentina <vcarmona911@soyudemellin.edu.co>
2024-04-18 00:17:58 -0500

    se realizan las actualizaciones

--- src/Logic/TaxLogic.py
+++ src/Logic/TaxLogic.py
@@ -36,9 +36,18 @@ class IncoherentFiguresExpection( Exception ):
 
 
 #LOGICA
-    
+class TaxInformation:
+    def __init__(self, total_labor_income_per_year: int, other_taxable_income_per_year: int, other_non_taxable_income_per_year: int, source_retention_value_per_year: int, mortgage_loan_payment_per_year: int, donation_value_per_year: int, educational_expenses_per_year: int ):    
+        self.total_labor_income_per_year = total_labor_income_per_year
+        self.other_taxable_income_per_year = other_taxable_income_per_year
+        self.other_non_taxable_income_per_year = other_non_taxable_income_per_year
+        self.source_retention_value_per_year = source_retention_value_per_year
+        self.mortgage_loan_payment_per_year = mortgage_loan_payment_per_year
+        self.donation_value_per_year = donation_value_per_year
+        self.educational_expenses_per_year = educational_expenses_per_year
+
 #Calcular el valor del impuesto a pagar
-def calculateTax(total_labor_income_per_year: int, other_taxable_income_per_year: int, other_non_taxable_income_per_year: int, source_retention_value_per_year: int, mortgage_loan_payment_per_year: int, donation_value_per_year: int, educational_expenses_per_year: int):
+def calculateTax(objeto):
     
     """
     total_labor_income_per_year: Total ingresos laborales al año
@@ -66,28 +75,39 @@ def calculateTax(total_labor_income_per_year: int, other_taxable_income_per_year
     #Limite de cifras
     NUMBERLIMIT = 6
 
+    # Definir los rangos y tasas impositivas como una lista de tuplas
+    TAX_RATES = [
+        (0, 1090, 0),
+        (1090, 1700, 0.19),
+        (1700, 4100, 0.28),
+        (4100, 8670, 0.33),
+        (8670, 18970, 0.35),
+        (18970, 31000, 0.37),
+        (31000, float('inf'), 0.39)
+    ]
+
 
     #CONTROL DE ERROR
-    if type(total_labor_income_per_year) == str or type(other_taxable_income_per_year) == str or type(other_non_taxable_income_per_year) == str or type(source_retention_value_per_year) == str or type(mortgage_loan_payment_per_year) == str or type(donation_value_per_year) == str or type(educational_expenses_per_year) == str:
+    if type(objeto.total_labor_income_per_year) == str or type(objeto.other_taxable_income_per_year) == str or type(objeto.other_non_taxable_income_per_year) == str or type(objeto.source_retention_value_per_year) == str or type(objeto.mortgage_loan_payment_per_year) == str or type(objeto.donation_value_per_year) == str or type(objeto.educational_expenses_per_year) == str:
         raise InvalidEntryException("Ingresaste un valor invalido.")
     
-    if (total_labor_income_per_year < 0 or other_non_taxable_income_per_year < 0) and other_taxable_income_per_year < 0:
+    if (objeto.total_labor_income_per_year < 0 or objeto.other_non_taxable_income_per_year < 0) and objeto.other_taxable_income_per_year < 0:
         raise NegativeValueEnteredException("No puedes ingresar valor negativos en esta casilla.")
 
-    if source_retention_value_per_year > total_labor_income_per_year:
+    if objeto.source_retention_value_per_year > objeto.total_labor_income_per_year:
         raise HigherIncomeRetentionException("La retencion en la fuente no puede ser mayor al valor total de ingresos.")
 
-    if len(str(total_labor_income_per_year)) > MAXDIGITS or len(str(other_taxable_income_per_year)) > MAXDIGITS or len(str(other_non_taxable_income_per_year)) > MAXDIGITS or len(str(source_retention_value_per_year)) > MAXDIGITS or len(str(mortgage_loan_payment_per_year)) > MAXDIGITS or len(str(donation_value_per_year)) > MAXDIGITS or len(str(educational_expenses_per_year)) > MAXDIGITS:
+    if len(str(objeto.total_labor_income_per_year)) > MAXDIGITS or len(str(objeto.other_taxable_income_per_year)) > MAXDIGITS or len(str(objeto.other_non_taxable_income_per_year)) > MAXDIGITS or len(str(objeto.source_retention_value_per_year)) > MAXDIGITS or len(str(objeto.mortgage_loan_payment_per_year)) > MAXDIGITS or len(str(objeto.donation_value_per_year)) > MAXDIGITS or len(str(objeto.educational_expenses_per_year)) > MAXDIGITS:
         raise DigitsVeryLargeError("Numeros ingresados muy grandes, exceden el valor normal a calcular.")
     
-    if other_taxable_income_per_year == 0 and other_non_taxable_income_per_year == 0:
+    if objeto.other_taxable_income_per_year == 0 and objeto.other_non_taxable_income_per_year == 0:
         raise AssetsNotEnteredException("Activos no agregados, ingrese activos para continuar.")
 
-    if total_labor_income_per_year == 0:
+    if objeto.total_labor_income_per_year == 0:
         raise DataNotAggregatedError("Error! Datos obligatorios no agregados. Ingreselos para poder continuar.")
 
-    if total_labor_income_per_year > 0 and total_labor_income_per_year < 1:
-        num_str = str(total_labor_income_per_year)
+    if objeto.total_labor_income_per_year > 0 and objeto.total_labor_income_per_year < 1:
+        num_str = str(objeto.total_labor_income_per_year)
 
         if '.' in num_str:
             decimal_part = num_str.split('.')[1]
@@ -97,17 +117,18 @@ def calculateTax(total_labor_income_per_year: int, other_taxable_income_per_year
     
 
     #Calcular el valor a la seguridad social en el año y el aporte a la pension en el año
-    social_security_payment_in_the_year = ( total_labor_income_per_year * SOCIALSECURITYPERCENTAGE ) / 100
-    pension_contributions_in_the_year = ( total_labor_income_per_year * PENSIONCONTRIBUTIONPERCENTAGE ) / 100
+    social_security_payment_in_the_year = ( objeto.total_labor_income_per_year * SOCIALSECURITYPERCENTAGE ) / 100
+    pension_contributions_in_the_year = ( objeto.total_labor_income_per_year * PENSIONCONTRIBUTIONPERCENTAGE ) / 100
 
     #Calcular Total de Ingresos NO Gravables
-    total_untaxed_income = other_non_taxable_income_per_year
+    pension_contributions_in_the_year = ( objeto.total_labor_income_per_year * PENSIONCONTRIBUTIONPERCENTAGE ) / 100
+    total_untaxed_income = objeto.other_non_taxable_income_per_year
 
     #Calcular el Total de Ingresos Gravados
-    total_taxed_income =  ( total_labor_income_per_year + other_taxable_income_per_year + other_non_taxable_income_per_year) - total_untaxed_income
+    total_taxed_income =  ( objeto.total_labor_income_per_year + objeto.other_taxable_income_per_year + objeto.other_non_taxable_income_per_year) - total_untaxed_income
 
     #Calcular Total Costos Deducibles
-    total_deductible_costs = social_security_payment_in_the_year + pension_contributions_in_the_year + mortgage_loan_payment_per_year + donation_value_per_year + educational_expenses_per_year
+    total_deductible_costs = social_security_payment_in_the_year + pension_contributions_in_the_year + objeto.mortgage_loan_payment_per_year + objeto.donation_value_per_year + objeto.educational_expenses_per_year
 
 
     #Validacion deducibles no sean menores que cero
@@ -118,7 +139,13 @@ def calculateTax(total_labor_income_per_year: int, other_taxable_income_per_year
     #Calcular las unidades de valor tributario
     tax_value_units = total_taxed_income / 46076
 
-    #Calculamos tasa impositiva
+    # Iterar sobre las tuplas de rangos y tasas impositivas para encontrar la tasa impositiva adecuada
+    for lower_limit, upper_limit, rate in TAX_RATES:
+        if lower_limit < tax_value_units <= upper_limit:
+            tax_rate = rate
+            break
+
+    """Calculamos tasa impositiva
     if tax_value_units <= 1090:
         tax_rate = 0
     elif tax_value_units > 1090 and tax_value_units <= 1700:
@@ -133,12 +160,12 @@ def calculateTax(total_labor_income_per_year: int, other_taxable_income_per_year
         tax_rate = 0.37
     elif tax_value_units > 31000:
         tax_rate = 0.39
-
+    """
     #Calcular el valor a pagar por impuestos de rentas
     if tax_rate == 0:
         amount_to_pay_income_taxes = 0
     else:
-        amount_to_pay_income_taxes = (( total_taxed_income - total_deductible_costs ) * tax_rate ) - source_retention_value_per_year
+        amount_to_pay_income_taxes = (( total_taxed_income - total_deductible_costs ) * tax_rate ) - objeto.source_retention_value_per_year
 
     #Lista de resultados
     results = []
